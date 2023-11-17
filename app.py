c255ab5bf4d813d305d68f8bd8f5fcb911c0ba2d
ff ff <fayooz050050@gmail.com>
2023-11-18 02:44:44 +0300

    4

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def result():
         return render_template('error.html', error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
